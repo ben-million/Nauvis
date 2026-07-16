84b727dffcf5d1f641e1192c067844f36967206c
ben-million <ben@million.dev>
2026-07-15 19:23:43 -0700

    Initial Commit

---
+++ Nauvis/NauvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  NauvisApp.swift
+//  Nauvis
+//
+//  Created by Ben on 7/15/26.
+//
+
+import SwiftUI
+
+@main
+struct NauvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
